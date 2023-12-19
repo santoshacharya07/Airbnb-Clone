5d6ebe50cdc0556285ffc6e5993f0fc6a0dd17c2
Santosh Acharya <santoshacharya747@gmail.com>
2023-12-19 13:41:26 +0545

    Initial Commit

---
+++ Airbnb Clone/Airbnb_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Airbnb_CloneApp.swift
+//  Airbnb Clone
+//
+//  Created by Santosh Acharya on 19/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Airbnb_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
